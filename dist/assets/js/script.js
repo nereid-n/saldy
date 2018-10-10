ff41691123166192c1e375c3426b2c7f0f68397a
nereid <nas.kuleshova@gmail.com>
2018-10-10 17:56:40 +0300

    chagne header and menu

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -1,8 +1,38 @@
-"use strict";
+'use strict';
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+$(document).ready(function () {
 
-var Shared = function Shared() {
-    _classCallCheck(this, Shared);
-};
+  // Elements to inject
+  if ($('.inject-me').length > 0) {
+    var mySVGsToInject = document.querySelectorAll('img.inject-me');
+
+    // Options
+    var injectorOptions = {
+      evalScripts: 'once',
+      pngFallback: 'assets/png',
+      each: function each(svg) {}
+    };
+
+    SVGInjector(mySVGsToInject, injectorOptions, function (totalSVGsInjected) {
+      // Callback after all SVGs are injected
+      if ($(window).width() > 959) {
+        var topSideHeight = $('.headerCover').outerHeight(true);
+        $('#main section').first().css({ "padding-top": topSideHeight });
+      }
+    });
+  }
+
+  if ($('.select').length > 0) {
+    $('.select').select2({
+      dropdownParent: $('.wrap-select')
+    }).on("select2:open", function () {
+      $(".select2-results ul.select2-results__options").unbind("mousewheel");
+      $('.select2-results').mCustomScrollbar({
+        axis: "y",
+        scrollEasing: "linear",
+        scrollInertia: 150
+      });
+    });
+  }
+});
 //# sourceMappingURL=script.js.map
